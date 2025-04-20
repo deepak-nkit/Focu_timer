ebf7cb96c509a2533380b274d47d76fa8184fe8d
Deepak Saini <ds954642@gmail.com>
2025-04-20 21:58:56 +0530

    stop timer working

--- script.js
+++ script.js
@@ -1,14 +1,44 @@
-// let resume = document.getElementById('resume-btn');
 let btn = document.getElementById("start-btn");
+let stopBtn = document.getElementById("stop-btn");
 
-btn.addEventListener("click" , (evnt)=>{
-  evnt.preventDefault();
-  let timeInputs = document.querySelectorAll(".time-input");
-  let hrs = timeInputs[0].value;
-  let min = timeInputs[1].value;
-  let sec = timeInputs[2].value;
-  // timeInputs.forEach((time , index) => {
-  //   console.log(`Input Index${index}:`, time.value)
-  // })
-  console.log(`Hrs: ${hrs} ,Min: ${min} ,Sec: ${sec}`);
-})
+let timer;
+function handleTime(minutes , times){
+	// console.log("in minut fxn : ",minutes)
+	minutes--;
+	times[1].value = minutes
+	return minutes;
+}
+
+function startTimer(evnt) {
+	evnt.preventDefault();
+	stopBtn.hidden = false;
+	let timeInputs = document.querySelectorAll(".time-input");
+	let hrs = timeInputs[0].value;
+	let min = timeInputs[1].value;
+	let sec = timeInputs[2].value;
+
+	if(!timer){
+	  timer = setInterval(() => {
+			timeInputs[2].value = sec;
+			sec--;
+		if(sec === -1 && min != 0){
+			sec = 59;
+			let remaining_min	=	handleTime(min , timeInputs);
+			min = remaining_min;
+		}else if(min === 0 && hrs != 0){
+			hrs--;
+			timeInputs[0].value = hrs
+			min = 59;
+			timeInputs[1].value = min
+		}
+		}, 1000);
+}}
+
+function stopTimer(event){
+		event.preventDefault();
+		clearInterval(timer);
+		timer = null;
+}
+
+btn.addEventListener("click" , startTimer);
+stopBtn.addEventListener("click", stopTimer);
